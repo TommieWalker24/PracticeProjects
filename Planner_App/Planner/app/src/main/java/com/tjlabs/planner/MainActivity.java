db3a85b811535b02fb9a36496009e99175e22283
TommieWalker24 <57117928+TommieWalker24@users.noreply.github.com>
2020-07-23 16:02:02 -0500

    new project

---
+++ Planner_App/Planner/app/src/main/java/com/tjlabs/planner/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tjlabs.planner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    //define globals here
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
